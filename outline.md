f3fd070a7543a880e5fc691dff5d689509dc9b61
Holly Lloyd <hollylawly@gmail.com>
2020-12-13 17:18:40 -0800

    initializing

---
+++ outline.md
@@ -0,0 +1,29 @@
+# Authenticating Svelte Apps with Auth0
+
+**TL;DR:**
+
+## What we are to build
+
+## Prerequisites
+
+## Creating the Auth0 Project
+
+## Scaffolding the Svelte project
+
+## Setting up Authentication with Auth0
+
+### Creating a Svelte store to hold authentication state
+
+### Creating an authentication service
+
+## Building the Task List
+
+### Creating a List Item Component
+
+### Building the Task List page
+
+### Adding authentication to the Task List page
+
+## Running the Application
+
+## Conclusion
---
+++ src/store.js
@@ -0,0 +1,22 @@
+import {writable, derived} from "svelte/store";
+
+
+export const loading =  writable(true);
+export const isAuthenticated = writable(false);
+export const user =  writable({});
+export const popupOpen = writable(false)
+export const error = writable();
+
+export const tasks = writable([]);
+
+export const user_tasks = derived([tasks, user], ([$tasks, $user]) => {
+
+    let logged_in_user_tasks = [];
+
+    if($user && $user.email){
+        logged_in_user_tasks = $tasks.filter((task) => task.user === $user.email)
+    }
+
+    return logged_in_user_tasks;
+});
+
